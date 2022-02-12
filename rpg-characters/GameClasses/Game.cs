737a72dd48cb14f44795a8732d86db3a01795928
arttuhar <arttuhar@gmail.com>
2022-02-12 11:15:19 +0200

    FEAT: Display hero class list, ask to select hero class

--- rpg-characters/GameClasses/Game.cs
+++ rpg-characters/GameClasses/Game.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace rpg_characters.GameClasses
 {
@@ -8,16 +11,17 @@ namespace rpg_characters.GameClasses
         {
             StartMessage();
             HeroName();
+            HeroClass();
             
         }
-
+        
         public static void StartMessage()
         {
             string header = "Get ready, adventure begins now!";
             Console.WriteLine(header);
 
         }
-
+        
         public static string HeroName()
         {
             string askName = "Greetings adventurer! What is your name?";
@@ -27,6 +31,40 @@ namespace rpg_characters.GameClasses
             
             return name;
 
+        }
+
+        public static int HeroClass()
+        {
+            List<string> heroClassOptions = new List<string>();
+            heroClassOptions.Add("1 - Mage");
+            heroClassOptions.Add("2 - Ranger");
+            heroClassOptions.Add("3 - Rogue");
+            heroClassOptions.Add("4 - Warrior");
+
+            foreach (string heroClassOption in heroClassOptions)
+            {
+                Console.WriteLine(heroClassOption);
+            }
+
+            string askClass = "Select your class:";
+            Console.WriteLine(askClass);
+            int selectedClass = int.Parse(Console.ReadLine());
+
+            while (selectedClass < 1 || selectedClass > 4)
+            {
+                Console.WriteLine("Class not exist");
+
+                foreach (string heroClassOption in heroClassOptions)
+                {
+                    Console.WriteLine(heroClassOption);
+                }
+
+                selectedClass = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine(selectedClass);
+            return selectedClass;
         }
     }
 }
