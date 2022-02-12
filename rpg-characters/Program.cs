eb75f437d41b65a1e112c36d92b0af49dcce7d19
arttuhar <arttuhar@gmail.com>
2022-02-12 13:52:11 +0200

    REFACTOR: Simplify new expression while creating new hero

--- rpg-characters/Program.cs
+++ rpg-characters/Program.cs
@@ -7,16 +7,16 @@ namespace rpg_characters
     {
         static void Main(string[] args)
         {
-            Mage mageHero = new Mage("Arttu the Mage");
+            Mage mageHero = new("Arttu the Mage");
             Console.WriteLine($"{mageHero.HeroName}, {mageHero.HeroClass}, {mageHero.CurrentLevel}\n");
 
-            Ranger rangerHero = new Ranger("Arttu the Ranger");
+            Ranger rangerHero = new("Arttu the Ranger");
             Console.WriteLine($"{rangerHero.HeroName}, {rangerHero.HeroClass}, {rangerHero.CurrentLevel}\n");
 
-            Rogue rogueHero = new Rogue("Arttu the Rogue");
+            Rogue rogueHero = new("Arttu the Rogue");
             Console.WriteLine($"{rogueHero.HeroName}, {rogueHero.HeroClass}, {rogueHero.CurrentLevel} \n");
 
-            Warrior warriorHero = new Warrior("Arttu the Warrior");
+            Warrior warriorHero = new("Arttu the Warrior");
             Console.WriteLine($"{warriorHero.HeroName}, {warriorHero.HeroClass}, {rogueHero.CurrentLevel}");
         }
     }
