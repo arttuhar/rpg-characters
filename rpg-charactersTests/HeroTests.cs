a2cbaa48eb5d00bd0d722a93cd1fc8e61a7c3561
arttuhar <arttuhar@gmail.com>
2022-02-15 11:38:00 +0200

    TEST: Get each class base primary attributes

--- rpg-charactersTests/HeroTests.cs
+++ rpg-charactersTests/HeroTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using rpg_characters.Helpers;
 using rpg_characters.Heroes;
 using Xunit;
 
@@ -28,10 +29,66 @@ namespace rpg_charactersTests
             mageHero.LevelUp();
             int expected = 2;
 
-            //Act
+            // Act
             int actual = mageHero.CurrentLevel;
 
-            //Assert
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BasePrimaryAttributes_MageAttributes_GetProperMageDefaultAttributes()
+        {
+            // Arrange
+            Mage mageHero = new("Mage");
+            PrimaryAttributes expected = new() { Strength = 1, Dexterity = 1, Intelligence = 8};
+
+            // Act
+            PrimaryAttributes actual = mageHero.BasePrimaryAttributes;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BasePrimaryAttributes_RangerAttributes_GetProperRangerDefaultAttributes()
+        {
+            // Arrange
+            Ranger rangerHero = new("Ranger");
+            PrimaryAttributes expected = new() { Strength = 1, Dexterity = 7, Intelligence = 1 };
+
+            // Act
+            PrimaryAttributes actual = rangerHero.BasePrimaryAttributes;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BasePrimaryAttributes_RogueAttributes_GetProperRogueDefaultAttributes()
+        {
+            // Arrange
+            Rogue rogueHero = new("Rogue");
+            PrimaryAttributes expected = new() { Strength = 2, Dexterity = 6, Intelligence = 1 };
+
+            // Act
+            PrimaryAttributes actual = rogueHero.BasePrimaryAttributes;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BasePrimaryAttributes_WarriorAttributes_GetProperWarriorDefaultAttributes()
+        {
+            // Arrange
+            Warrior warriorHero = new("Warrior");
+            PrimaryAttributes expected = new() { Strength = 5, Dexterity = 2, Intelligence = 1 };
+
+            // Act
+            PrimaryAttributes actual = warriorHero.BasePrimaryAttributes;
+
+            // Assert
             Assert.Equal(expected, actual);
         }
     }
