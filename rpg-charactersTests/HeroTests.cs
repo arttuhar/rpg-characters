081539ad4efb76a184bb1d8503de791b71605839
arttuhar <arttuhar@gmail.com>
2022-02-15 09:52:11 +0200

    TEST: Create new hero

---
+++ rpg-charactersTests/HeroTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using rpg_characters.Heroes;
+using Xunit;
+
+namespace rpg_charactersTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Constructor_CreateHero_HeroIsLevel1()
+        {
+            // Arrange
+            Mage mageHero = new("Mage");
+            int expected = 1;
+
+            // Act
+            int actual = mageHero.CurrentLevel;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
